4b4b710ead9954cf8eaefb6cae83e1c63bf1e4bb
SDEVROEDE <SDEVROEDE@PIGG04-02>
2017-10-12 15:34:59 +0200

    Button

--- SpritesheetMaker/SpriteSheetMaker.Designer.cs
+++ SpritesheetMaker/SpriteSheetMaker.Designer.cs
@@ -28,20 +28,45 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
-			// 
-			// SpritesheetMaker
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
-			this.Name = "SpritesheetMaker";
-			this.Text = "SpritesheetMaker";
-			this.ResumeLayout(false);
+            this.browseButton = new System.Windows.Forms.Button();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(13, 13);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse..";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(13, 43);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(259, 20);
+            this.pathBox.TabIndex = 1;
+            // 
+            // SpritesheetMaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.browseButton);
+            this.Name = "SpritesheetMaker";
+            this.Text = "SpritesheetMaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox pathBox;
+    }
 }
 
--- SpritesheetMaker/SpriteSheetMaker.cs
+++ SpritesheetMaker/SpriteSheetMaker.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
 
 namespace SpritesheetMaker
 {
@@ -17,6 +12,30 @@ namespace SpritesheetMaker
 			InitializeComponent();
 		}
 
+        private string[] filesList;
+        public List<string> imagesList = new List<string>();
 
-	}
+        private readonly string[] allowedExts = { ".png", ".jpg", ".tga" };
+
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void GetFilesInFolder()
+        {
+            filesList = Directory.GetFiles(pathBox.Text);
+            for (int i = 0; i < filesList.Length; i++)
+            {
+                for (int y = 0; y < allowedExts.Length; y++)
+                {
+                    if (Path.GetExtension(filesList[i]) == allowedExts[y])
+                    {
+                        imagesList.Add(filesList[i]);
+                    }
+                }
+            }
+        }
+    }
 }
