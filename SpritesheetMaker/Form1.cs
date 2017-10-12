47df0131ffee0cbeb1632d89a37208279bdf9ddb
LVALET <LVALET@PIGG04-01>
2017-10-12 14:42:49 +0200

    added spritesheet maker

---
+++ SpritesheetMaker/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpritesheetMaker
+{
+	public partial class SpritesheetMaker : Form
+	{
+		public SpritesheetMaker()
+		{
+			InitializeComponent();
+		}
+
+
+	}
+}
