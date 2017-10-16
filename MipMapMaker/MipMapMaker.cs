e6fb269d1562e16ed5c2a3cfd9577c0ab51cdc4a
LVALET <LVALET@PIGG04-01>
2017-10-16 16:12:41 +0200

    image lib

---
+++ MipMapMaker/MipMapMaker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MipMapMaker
+{
+	public partial class MipMapMaker : Form
+	{
+		public MipMapMaker()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load( object sender, EventArgs e )
+		{
+
+		}
+
+
+	}
+}
