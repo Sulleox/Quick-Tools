ff6b88a733e26825610169df34c299e7d98853db
LVALET <LVALET@PIGG04-01>
2017-10-17 16:47:55 +0200

    Added tooltips

--- SpritesheetMaker/SpriteSheetMaker.Designer.cs
+++ SpritesheetMaker/SpriteSheetMaker.Designer.cs
@@ -28,146 +28,150 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpritesheetMaker));
-            this.BrowsingButton = new System.Windows.Forms.Button();
-            this.SpritesFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
-            this.spritepathBox = new System.Windows.Forms.TextBox();
-            this.extensionBox = new System.Windows.Forms.ComboBox();
-            this.nameBox = new System.Windows.Forms.TextBox();
-            this.overwriteBox = new System.Windows.Forms.CheckBox();
-            this.outpoutPathBox = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.outpoutFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
-            this.createButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.oneFolderCheck = new System.Windows.Forms.CheckBox();
-            this.SuspendLayout();
-            // 
-            // BrowsingButton
-            // 
-            this.BrowsingButton.Location = new System.Drawing.Point(138, 15);
-            this.BrowsingButton.Name = "BrowsingButton";
-            this.BrowsingButton.Size = new System.Drawing.Size(75, 23);
-            this.BrowsingButton.TabIndex = 0;
-            this.BrowsingButton.Text = "Browse";
-            this.BrowsingButton.UseVisualStyleBackColor = true;
-            this.BrowsingButton.Click += new System.EventHandler(this.BrowsingButton_Click);
-            // 
-            // spritepathBox
-            // 
-            this.spritepathBox.Location = new System.Drawing.Point(12, 44);
-            this.spritepathBox.Name = "spritepathBox";
-            this.spritepathBox.Size = new System.Drawing.Size(201, 20);
-            this.spritepathBox.TabIndex = 1;
-            // 
-            // extensionBox
-            // 
-            this.extensionBox.FormattingEnabled = true;
-            this.extensionBox.Items.AddRange(new object[] {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpritesheetMaker));
+			this.BrowsingButton = new System.Windows.Forms.Button();
+			this.SpritesFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+			this.spritepathBox = new System.Windows.Forms.TextBox();
+			this.extensionBox = new System.Windows.Forms.ComboBox();
+			this.nameBox = new System.Windows.Forms.TextBox();
+			this.overwriteBox = new System.Windows.Forms.CheckBox();
+			this.outpoutPathBox = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.outpoutFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+			this.createButton = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.oneFolderCheck = new System.Windows.Forms.CheckBox();
+			this.oneFolderTooltip = new System.Windows.Forms.ToolTip(this.components);
+			this.SuspendLayout();
+			// 
+			// BrowsingButton
+			// 
+			this.BrowsingButton.Location = new System.Drawing.Point(138, 15);
+			this.BrowsingButton.Name = "BrowsingButton";
+			this.BrowsingButton.Size = new System.Drawing.Size(75, 23);
+			this.BrowsingButton.TabIndex = 0;
+			this.BrowsingButton.Text = "Browse";
+			this.BrowsingButton.UseVisualStyleBackColor = true;
+			this.BrowsingButton.Click += new System.EventHandler(this.BrowsingButton_Click);
+			// 
+			// spritepathBox
+			// 
+			this.spritepathBox.Location = new System.Drawing.Point(12, 44);
+			this.spritepathBox.Name = "spritepathBox";
+			this.spritepathBox.Size = new System.Drawing.Size(201, 20);
+			this.spritepathBox.TabIndex = 1;
+			// 
+			// extensionBox
+			// 
+			this.extensionBox.FormattingEnabled = true;
+			this.extensionBox.Items.AddRange(new object[] {
             "png",
             "bmp",
             "jpg"});
-            this.extensionBox.Location = new System.Drawing.Point(153, 70);
-            this.extensionBox.Name = "extensionBox";
-            this.extensionBox.Size = new System.Drawing.Size(60, 21);
-            this.extensionBox.TabIndex = 2;
-            this.extensionBox.Text = "png";
-            // 
-            // nameBox
-            // 
-            this.nameBox.Location = new System.Drawing.Point(12, 70);
-            this.nameBox.Name = "nameBox";
-            this.nameBox.Size = new System.Drawing.Size(135, 20);
-            this.nameBox.TabIndex = 3;
-            this.nameBox.Text = "SpriteSheetName";
-            // 
-            // overwriteBox
-            // 
-            this.overwriteBox.AutoSize = true;
-            this.overwriteBox.Location = new System.Drawing.Point(12, 171);
-            this.overwriteBox.Name = "overwriteBox";
-            this.overwriteBox.Size = new System.Drawing.Size(71, 17);
-            this.overwriteBox.TabIndex = 4;
-            this.overwriteBox.Text = "Overwrite";
-            this.overwriteBox.UseVisualStyleBackColor = true;
-            // 
-            // outpoutPathBox
-            // 
-            this.outpoutPathBox.Location = new System.Drawing.Point(12, 145);
-            this.outpoutPathBox.Name = "outpoutPathBox";
-            this.outpoutPathBox.Size = new System.Drawing.Size(201, 20);
-            this.outpoutPathBox.TabIndex = 5;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(138, 116);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Browse";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // createButton
-            // 
-            this.createButton.Location = new System.Drawing.Point(138, 191);
-            this.createButton.Name = "createButton";
-            this.createButton.Size = new System.Drawing.Size(75, 23);
-            this.createButton.TabIndex = 7;
-            this.createButton.Text = "Create !";
-            this.createButton.UseVisualStyleBackColor = true;
-            this.createButton.Click += new System.EventHandler(this.createButton_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Select Sprites Folder";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 121);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Select Outpout Folder";
-            // 
-            // oneFolderCheck
-            // 
-            this.oneFolderCheck.AutoSize = true;
-            this.oneFolderCheck.Location = new System.Drawing.Point(12, 191);
-            this.oneFolderCheck.Name = "oneFolderCheck";
-            this.oneFolderCheck.Size = new System.Drawing.Size(78, 17);
-            this.oneFolderCheck.TabIndex = 10;
-            this.oneFolderCheck.Text = "One Folder";
-            this.oneFolderCheck.UseVisualStyleBackColor = true;
-            this.oneFolderCheck.CheckedChanged += new System.EventHandler(this.oneFolderCheck_CheckedChanged);
-            // 
-            // SpritesheetMaker
-            // 
-            this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(223, 226);
-            this.Controls.Add(this.oneFolderCheck);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.createButton);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.outpoutPathBox);
-            this.Controls.Add(this.overwriteBox);
-            this.Controls.Add(this.nameBox);
-            this.Controls.Add(this.extensionBox);
-            this.Controls.Add(this.spritepathBox);
-            this.Controls.Add(this.BrowsingButton);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "SpritesheetMaker";
-            this.Text = "SpriteSheet Maker";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.extensionBox.Location = new System.Drawing.Point(153, 70);
+			this.extensionBox.Name = "extensionBox";
+			this.extensionBox.Size = new System.Drawing.Size(60, 21);
+			this.extensionBox.TabIndex = 2;
+			this.extensionBox.Text = "png";
+			// 
+			// nameBox
+			// 
+			this.nameBox.Location = new System.Drawing.Point(12, 70);
+			this.nameBox.Name = "nameBox";
+			this.nameBox.Size = new System.Drawing.Size(135, 20);
+			this.nameBox.TabIndex = 3;
+			this.nameBox.Text = "SpriteSheetName";
+			// 
+			// overwriteBox
+			// 
+			this.overwriteBox.AutoSize = true;
+			this.overwriteBox.Location = new System.Drawing.Point(12, 171);
+			this.overwriteBox.Name = "overwriteBox";
+			this.overwriteBox.Size = new System.Drawing.Size(71, 17);
+			this.overwriteBox.TabIndex = 4;
+			this.overwriteBox.Text = "Overwrite";
+			this.oneFolderTooltip.SetToolTip(this.overwriteBox, "Force overwrite if spritesheets already exist");
+			this.overwriteBox.UseVisualStyleBackColor = true;
+			// 
+			// outpoutPathBox
+			// 
+			this.outpoutPathBox.Location = new System.Drawing.Point(12, 145);
+			this.outpoutPathBox.Name = "outpoutPathBox";
+			this.outpoutPathBox.Size = new System.Drawing.Size(201, 20);
+			this.outpoutPathBox.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(138, 116);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Browse";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// createButton
+			// 
+			this.createButton.Location = new System.Drawing.Point(138, 191);
+			this.createButton.Name = "createButton";
+			this.createButton.Size = new System.Drawing.Size(75, 23);
+			this.createButton.TabIndex = 7;
+			this.createButton.Text = "Create !";
+			this.createButton.UseVisualStyleBackColor = true;
+			this.createButton.Click += new System.EventHandler(this.createButton_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(104, 13);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Select Sprites Folder";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 121);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(110, 13);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Select Outpout Folder";
+			// 
+			// oneFolderCheck
+			// 
+			this.oneFolderCheck.AutoSize = true;
+			this.oneFolderCheck.Location = new System.Drawing.Point(12, 191);
+			this.oneFolderCheck.Name = "oneFolderCheck";
+			this.oneFolderCheck.Size = new System.Drawing.Size(78, 17);
+			this.oneFolderCheck.TabIndex = 10;
+			this.oneFolderCheck.Text = "One Folder";
+			this.oneFolderTooltip.SetToolTip(this.oneFolderCheck, "If the folder contains multiple spritesheets");
+			this.oneFolderCheck.UseVisualStyleBackColor = true;
+			this.oneFolderCheck.CheckedChanged += new System.EventHandler(this.oneFolderCheck_CheckedChanged);
+			// 
+			// SpritesheetMaker
+			// 
+			this.BackColor = System.Drawing.SystemColors.ControlLight;
+			this.ClientSize = new System.Drawing.Size(223, 226);
+			this.Controls.Add(this.oneFolderCheck);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.createButton);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.outpoutPathBox);
+			this.Controls.Add(this.overwriteBox);
+			this.Controls.Add(this.nameBox);
+			this.Controls.Add(this.extensionBox);
+			this.Controls.Add(this.spritepathBox);
+			this.Controls.Add(this.BrowsingButton);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "SpritesheetMaker";
+			this.Text = "SpriteSheet Maker";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -189,6 +193,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.CheckBox oneFolderCheck;
-    }
+		private System.Windows.Forms.ToolTip oneFolderTooltip;
+	}
 }
 
