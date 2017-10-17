1abe3e71926b8f5bd35baa6cb0c7697564061abe
Louis Valet <a.louisvalet@gmail.com>
2017-10-17 20:20:43 +0200

    fixed miip map packer

---
+++ MipMapMaker/MipMakerForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MipMapMaker
+{
+	partial class MipMakerForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if ( disposing && ( components != null ) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.browseButton = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.pathBox = new System.Windows.Forms.TextBox();
+			this.confirmButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// browseButton
+			// 
+			this.browseButton.Location = new System.Drawing.Point(12, 12);
+			this.browseButton.Name = "browseButton";
+			this.browseButton.Size = new System.Drawing.Size(75, 23);
+			this.browseButton.TabIndex = 0;
+			this.browseButton.Text = "Browse...";
+			this.browseButton.UseVisualStyleBackColor = true;
+			this.browseButton.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// pathBox
+			// 
+			this.pathBox.Location = new System.Drawing.Point(11, 41);
+			this.pathBox.Name = "pathBox";
+			this.pathBox.Size = new System.Drawing.Size(208, 20);
+			this.pathBox.TabIndex = 1;
+			// 
+			// confirmButton
+			// 
+			this.confirmButton.Location = new System.Drawing.Point(144, 67);
+			this.confirmButton.Name = "confirmButton";
+			this.confirmButton.Size = new System.Drawing.Size(75, 23);
+			this.confirmButton.TabIndex = 2;
+			this.confirmButton.Text = "Convert";
+			this.confirmButton.UseVisualStyleBackColor = true;
+			this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+			// 
+			// MipMakerForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(235, 106);
+			this.Controls.Add(this.confirmButton);
+			this.Controls.Add(this.pathBox);
+			this.Controls.Add(this.browseButton);
+			this.Name = "MipMakerForm";
+			this.Text = "MipMap Packer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button browseButton;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.TextBox pathBox;
+		private System.Windows.Forms.Button confirmButton;
+	}
+}
--- MipMapMaker/MipMakerForm.cs
+++ MipMapMaker/MipMakerForm.cs
@@ -1,25 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Media.Imaging;
 using CSharpImageLibrary;
 using System.IO;
-using System.Drawing.Imaging;
 
 namespace MipMapMaker
 {
-	public partial class MipMakerform : Form
+	public partial class MipMakerForm : Form
 	{
-
 		private readonly string[] allowedExts = { ".png", ".jpg", ".tga" };
 
-		public MipMakerform()
+		public MipMakerForm()
 		{
 			InitializeComponent();
 		}
@@ -29,13 +22,17 @@ namespace MipMapMaker
 			if ( folderBrowserDialog1.ShowDialog() == DialogResult.OK )
 			{
 				pathBox.Text = folderBrowserDialog1.SelectedPath;
-				GetFilesInFolder();
 			}
 		}
 
-		private void GetFilesInFolder()
+		private void confirmButton_Click( object sender, EventArgs e )
 		{
-			string[] fileNames = Directory.GetFiles( pathBox.Text );
+			ProcessFolder();
+		}
+
+		private void ProcessFolder()
+		{
+			string[] fileNames = Directory.GetFiles( pathBox.Text ).Where( f => allowedExts.Contains( Path.GetExtension( f ) ) ).ToArray();
 
 			List<List<string>> output = new List<List<string>>();
 			int currentSubList = 0;
@@ -73,29 +70,11 @@ namespace MipMapMaker
 			for ( int i = 1 ; i < paths.Count ; i++ )
 			{
 				ImageEngineImage mipImage = new ImageEngineImage( paths[i] );
-				ImageFormats.ImageEngineFormatDetails mipFormat = new ImageFormats.ImageEngineFormatDetails( ImageEngineFormat.BMP );
-
-				int ByteCount = mipImage.BitCount / 8;
-				byte[] rawData = mipImage.Save( mipFormat, MipHandling.KeepTopOnly );
-				byte[] finalData = new byte[mipImage.Height * mipImage.Width * ByteCount];
-				int offset = rawData.Length - finalData.Length;
-
-				for ( int pixY = 0 ; pixY < mipImage.Height ; pixY++ )
-					for ( int pixX = 0 ; pixX < mipImage.Width ; pixX++ )
-						for ( int channel = 0 ; channel < ByteCount ; channel++ )
-						{
-							int outputIndex = ( ( mipImage.Height - pixY - 1 ) * mipImage.Width * ByteCount ) + ( pixX * ByteCount ) + channel;
-							int inputIndex = ( pixY * mipImage.Width * ByteCount ) + ( pixX * ByteCount ) + channel + offset;
-
-							finalData[outputIndex] = rawData[inputIndex];
-						}
-
-				MipMap mip = new MipMap( finalData, mipImage.Width, mipImage.Height, mipFormat );
+				MipMap mip = new MipMap( mipImage.MipMaps[0].Pixels, mipImage.Width, mipImage.Height, mipImage.FormatDetails );
 				outputImage.MipMaps.Add( mip );
 			}
-
 			ImageFormats.ImageEngineFormatDetails outputFormat = new ImageFormats.ImageEngineFormatDetails( ImageEngineFormat.DDS_DXT1 );
-			byte[] data = outputImage.Save( outputFormat, MipHandling.Default, mipToSave: paths.Count );
+			byte[] data = outputImage.Save( outputFormat, MipHandling.KeepExisting );
 
 			using ( var file = File.Create( outputPath ) )
 			{
