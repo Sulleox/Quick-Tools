62468af4e88b7814c4c1023948f343c41da28940
LVALET <LVALET@PIGG04-01>
2017-10-17 10:52:25 +0200

    test filename retirever

--- MipMapMaker/Form1.cs
+++ MipMapMaker/Form1.cs
@@ -12,67 +12,99 @@ using System.IO;
 
 namespace MipMapMaker
 {
-    public partial class Form1 : Form
-    {
+	public partial class Form1 : Form
+	{
 
-        private readonly string[] allowedExts = { ".png", ".jpg", ".tga" };
+		private readonly string[] allowedExts = { ".png", ".jpg", ".tga" };
 
-        public Form1()
-        {
-            InitializeComponent();
-        }
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
-            {
-                pathBox.Text = folderBrowserDialog1.SelectedPath;
-                GetFilesInFolder();
-            }
-        }
+		private void button1_Click( object sender, EventArgs e )
+		{
+			if ( folderBrowserDialog1.ShowDialog() == DialogResult.OK )
+			{
+				pathBox.Text = folderBrowserDialog1.SelectedPath;
+				GetFilesInFolder();
+			}
+		}
 
-        //Nomenclature des fichiers : NomDuFichier_Mip0
+		//Nomenclature des fichiers : NomDuFichier_Mip0
 
-        private void GetFilesInFolder()
-        {
-            string[] fileNames = Directory.GetFiles(pathBox.Text);
-            List<string> mipmap = new List<string>();
+		private void GetFilesInFolder()
+		{
+			string[] fileNames = Directory.GetFiles( pathBox.Text );
+			//List<string> mipmap = new List<string>();
 
-            for (int i = 0; i < fileNames.Length; i++)
-            {
-                if (i != 0)
-                {
-                    if (fileNames[i].Remove(fileNames.Length - 5) == fileNames[i - 1].Remove(fileNames.Length - 5))
-                    {
-                        mipmap.Add(fileNames[i]);
-                    }
-                    else
-                    {
-                        SaveDDS(mipmap, fileNames[i - 1].Remove(fileNames.Length - 5));
-                        mipmap.Clear();
-                        mipmap.Add(fileNames[i]);
-                    }
-                }
-                else
-                {
-                    mipmap.Add(fileNames[i]);
-                }
-            }
-        }
-        private void SaveDDS(List<string> paths, string outputPath)
-        {
-            ImageEngineImage outputImage = new ImageEngineImage(outputPath);
+			//for (int i = 0; i < fileNames.Length; i++)
+			//{
+			//    if (i != 0)
+			//    {
+			//        if (fileNames[i].Remove(fileNames.Length - 5) == fileNames[i - 1].Remove(fileNames.Length - 5))
+			//        {
+			//            mipmap.Add(fileNames[i]);
+			//        }
+			//        else
+			//        {
+			//            SaveDDS(mipmap, fileNames[i - 1].Remove(fileNames.Length - 5));
+			//            mipmap.Clear();
+			//            mipmap.Add(fileNames[i]);
+			//        }
+			//    }
+			//    else
+			//    {
+			//        mipmap.Add(fileNames[i]);
+			//    }
+			//}
 
-            for (int i = 0; i < paths.Count; i++)
-            {
-                ImageEngineImage mipImage = new ImageEngineImage(paths[i]);
-                MipMap mip = new MipMap(mipImage.OriginalData, mipImage.Width, mipImage.Height, mipImage.FormatDetails);
-                outputImage.MipMaps[i] = mip;
-            }
 
-            ImageFormats.ImageEngineFormatDetails outputFormat = new ImageFormats.ImageEngineFormatDetails(ImageEngineFormat.DDS_DXT3);
+			List<List<string>> output = new List<List<string>>();
+			int currentSubList = 0;
+			output.Add( new List<string>() );
+			output[0].Add( fileNames[0] );
 
-            outputImage.Save(outputPath, outputFormat, MipHandling.KeepExisting, mipToSave: paths.Count);
-        }
-    }
+			for ( int i = 1 ; i < fileNames ; i++ )
+			{
+				string original = output[currentSubList][0].Substring( 0, output[currentSubList][0].Length - output[currentSubList][0].LastIndexOf( '_' ) );
+				string input = fileNames[i].Substring( 0, fileNames[i].Length - fileNames[i].LastIndexOf( '_' ) );
+
+				if ( original == input )
+				{
+					output[currentSubList].Add( fileNames[i] );
+				}
+				else
+				{
+					currentSubList++;
+					output[currentSubList].Add( fileNames[i] );
+				}
+			}
+
+			for ( int j = 0 ; j < output.Count ; j++ )
+			{
+				for ( int k = 0 ; k < output[j].Count ; k++ )
+				{
+					Console.WriteLine( output[j][k] );
+
+				}
+			}
+		}
+
+		private void SaveDDS( List<string> paths, string outputPath )
+		{
+			ImageEngineImage outputImage = new ImageEngineImage( outputPath );
+
+			for ( int i = 0 ; i < paths.Count ; i++ )
+			{
+				ImageEngineImage mipImage = new ImageEngineImage( paths[i] );
+				MipMap mip = new MipMap( mipImage.OriginalData, mipImage.Width, mipImage.Height, mipImage.FormatDetails );
+				outputImage.MipMaps[i] = mip;
+			}
+
+			ImageFormats.ImageEngineFormatDetails outputFormat = new ImageFormats.ImageEngineFormatDetails( ImageEngineFormat.DDS_DXT3 );
+
+			outputImage.Save( outputPath, outputFormat, MipHandling.KeepExisting, mipToSave: paths.Count );
+		}
+	}
 }
