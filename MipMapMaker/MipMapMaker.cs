be97d027309261ed7303a8603dbfdd3d90389aa9
SDEVROEDE <SDEVROEDE@PIGG04-02>
2017-10-17 10:20:29 +0200

    Merge Scripts

--- MipMapMaker/MipMapMaker.cs
+++ MipMapMaker/MipMapMaker.cs
@@ -31,23 +31,24 @@ namespace MipMapMaker
 			}
 		}
 
-        private List<string> GetFilesInFolder()
+        //Nomenclature des fichiers : NomDuFichier_Mip0
+
+        private void GetFilesInFolder()
         {
             string[] fileNames = Directory.GetFiles(pathBox.Text);
             List<string> mipmap = new List<string>();
 
             for (int i = 0; i < fileNames.Length; i++)
             {
-                //NomDuFichier_Mip0
                 if (i != 0)
                 {
-                    if(fileNames[i].Remove(fileNames.Length - 5) != fileNames[i - 1].Remove(fileNames.Length - 5))
+                    if(fileNames[i].Remove(fileNames.Length - 5) == fileNames[i - 1].Remove(fileNames.Length - 5))
                     {
                         mipmap.Add(fileNames[i]);
                     }
                     else
                     {
-                        
+                        SaveDDS(mipmap, fileNames[i - 1].Remove(fileNames.Length - 5));
                         mipmap.Clear();
                         mipmap.Add(fileNames[i]);
                     }
@@ -57,7 +58,21 @@ namespace MipMapMaker
                     mipmap.Add(fileNames[i]);
                 }
             }
+        }
+        private void SaveDDS(List<string> paths, string outputPath)
+        {
+            ImageEngineImage outputImage = new ImageEngineImage(outputPath);
 
+            for (int i = 0; i < paths.Count; i++)
+            {
+                ImageEngineImage mipImage = new ImageEngineImage(paths[i]);
+                MipMap mip = new MipMap(mipImage.OriginalData, mipImage.Width, mipImage.Height, mipImage.FormatDetails);
+                outputImage.MipMaps[i] = mip;
+            }
+
+            ImageFormats.ImageEngineFormatDetails outputFormat = new ImageFormats.ImageEngineFormatDetails(ImageEngineFormat.DDS_DXT3);
+
+            outputImage.Save(outputPath, outputFormat, MipHandling.KeepExisting, mipToSave: paths.Count);
         }
     }
 }
