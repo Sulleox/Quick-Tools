6e2a76cf17bdbf4281f9de3f262f0e7e3c46807d
SDEVROEDE <SDEVROEDE@PIGG04-02>
2017-10-17 16:53:36 +0200

    Added Link to Github

--- SpritesheetMaker/SpriteSheetMaker.cs
+++ SpritesheetMaker/SpriteSheetMaker.cs
@@ -204,5 +204,10 @@ namespace SpritesheetMaker
         {
             nameBox.Enabled = !oneFolderCheck.Checked;
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Sulleox/Pole3DTools");
+        }
     }
 }
